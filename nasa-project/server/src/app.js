213db66f32e9514e6fda685cc5d6c7036008388b
Sivasai Silla <sivasai3920@gmail.com>
2024-05-27 04:15:36 +0530

    Delete

--- nasa-project/server/src/app.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const planetsRouter = require("./routes/planets/planets.router");
-const app = express();
-
-module.exports = app;
--- nasa-project/server/src/routes/planets/planets.controller.js
+++
@@ -1,7 +0,0 @@
-const planets = require("../../models/planets.model");
-
-function getAllPlanets(req, res) {
-  res.status(200).json(planets);
-}
-
-module.exports = { getAllPlanets };
